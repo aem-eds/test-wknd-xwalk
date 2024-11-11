a6afbde1f71104f73dc1b1d03a2a7ce1d0b4b39f
Navdeep Singh <navdeepsingh.26.2000@gmail.com>
2024-11-11 16:16:14 +0530

    update space constraint

--- blocks/offer/offer.js
+++ blocks/offer/offer.js
@@ -22,7 +22,7 @@ export default async function decorate(block) {
       if (masterData) {
         offer.title = masterData.title;
         offer.subtitle = masterData.subtitle;
-        offer.bannerimage = encodeURIComponent(masterData.bannerimage);
+        offer.bannerimage = masterData.bannerimage.replace(/ /g, '%20');
         offer.description = masterData.description;
         offer.ctalabel = masterData.ctalabel;
         offer.ctaurl = masterData.ctaurl;
@@ -30,7 +30,7 @@ export default async function decorate(block) {
         const masterData = contentfragment['jcr:content'].data.master;        
         offer.title = masterData.title;
         offer.subtitle = masterData.subtitle;
-        offer.bannerimage =  encodeURIComponent(masterData.bannerimage);
+        offer.bannerimage =  masterData.bannerimage.replace(/ /g, '%20');
         offer.description = masterData.description;
         offer.ctalabel = masterData.ctalabel;
         offer.ctaurl = masterData.ctaurl;
